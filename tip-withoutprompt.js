f79a9af1774ef36e9bcecfbb5dc8c5064bf655fc
Rami Ashtar <ramiashtar@Ramis-MacBook-Pro.local>
2016-03-08 21:59:06 -0500

    modified tip-withoutprompt to use enum for satisfaction

--- tip-withoutprompt.js
+++ tip-withoutprompt.js
@@ -1,17 +1,25 @@
-"use strict";
-exports.HST = 0.13;
+// Create a constant for HST
+var HST = 0.13;
+// Create a variable for the meal price
+// TODO this should be input from the user
 var mealPrice = 10;
-var satisfaction;
 // Satisfaction measured on 3 levels:
 // 0 = Waiter spat in your food, 0% tip
 // 1 = Unsatisfied, 10% tip
 // 2 = Satisified, 15% tip
 // 3 = Extremely Satisfied, 20% tip
+var satisfaction;
+(function (satisfaction) {
+    satisfaction[satisfaction["terrible"] = 0] = "terrible";
+    satisfaction[satisfaction["unsatisfied"] = 0.1] = "unsatisfied";
+    satisfaction[satisfaction["satisfied"] = 0.15] = "satisfied";
+    satisfaction[satisfaction["verySatisfied"] = 0.2] = "verySatisfied";
+})(satisfaction || (satisfaction = {}));
 console.log('Given that the meal price was ' + mealPrice);
-console.log('Waiter spat in my food: ' + calculateTip(mealPrice, 0));
-console.log('Unsatisfied: ' + calculateTip(mealPrice, 0.10));
-console.log('Satisfied: ' + calculateTip(mealPrice, 0.15));
-console.log('Extremely Satisfied: ' + calculateTip(mealPrice, 0.20));
+console.log('Waiter spat in my food: ' + calculateTip(mealPrice, satisfaction.terrible));
+console.log('Unsatisfied: ' + calculateTip(mealPrice, satisfaction.unsatisfied));
+console.log('Satisfied: ' + calculateTip(mealPrice, satisfaction.satisfied));
+console.log('Extremely Satisfied: ' + calculateTip(mealPrice, satisfaction.verySatisfied));
 function calculateTip(mealPrice, satisfaction) {
-    return mealPrice * (exports.HST + 1) * (satisfaction + 1);
+    return mealPrice * (HST + 1) * (satisfaction + 1);
 }
--- tip-withoutprompt.ts
+++ tip-withoutprompt.ts
@@ -1,20 +1,27 @@
-export const HST : number = 0.13;
+// Create a constant for HST
+const HST : number = 0.13;
+
+// Create a variable for the meal price
+// TODO this should be input from the user
 var mealPrice : number = 10;
-var satisfaction : number;
 
 // Satisfaction measured on 3 levels:
 // 0 = Waiter spat in your food, 0% tip
 // 1 = Unsatisfied, 10% tip
 // 2 = Satisified, 15% tip
 // 3 = Extremely Satisfied, 20% tip
+enum satisfaction {
+	terrible = 0,
+	unsatisfied = 0.10,
+	satisfied = 0.15,
+	verySatisfied = 0.20
+}
 
 console.log('Given that the meal price was ' + mealPrice);
-console.log('Waiter spat in my food: ' + calculateTip(mealPrice, 0));
-console.log('Unsatisfied: ' + calculateTip(mealPrice, 0.10));
-console.log('Satisfied: ' + calculateTip(mealPrice, 0.15));
-console.log('Extremely Satisfied: ' + calculateTip(mealPrice, 0.20));
-
-
+console.log('Waiter spat in my food: ' + calculateTip(mealPrice, satisfaction.terrible));
+console.log('Unsatisfied: ' + calculateTip(mealPrice, satisfaction.unsatisfied));
+console.log('Satisfied: ' + calculateTip(mealPrice, satisfaction.satisfied));
+console.log('Extremely Satisfied: ' + calculateTip(mealPrice, satisfaction.verySatisfied));
 
 function calculateTip(mealPrice:number, satisfaction:number) : number {
 	return mealPrice * (HST + 1) * (satisfaction + 1);
